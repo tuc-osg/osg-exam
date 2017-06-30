83fb2323aec6b91341e94eef41d8615b4bf5914b
Jafar <adonnen@gmail.com>
2017-06-30 16:16:06 -0700

    first Jafar commit

--- generate_exams.py
+++ generate_exams.py
@@ -8,7 +8,9 @@
 			Columns with "Matrikelnummer", "Vorname" and "Name"
 			...
 '''
-import os, csv, shutil, os.path
+import os, shutil, os.path
+from openpyxl import Workbook
+from openpyxl import load_workbook
 
 # Target folder for exam PDF files.
 folder="pdf"
@@ -40,11 +42,41 @@ try:
 except:
 	pass
 
-with open('teilnehmer.csv', 'rb') as csvfile:
-	reader = csv.reader(csvfile, delimiter=';')
-	for row in reader:
-		genexam(*row)
 
-# Also generate a blank one. LaTex needs some content, so we use gibberish white space.
-genexam('~','~','~')
+# with open('teilnehmer.csv', 'rb') as csvfile:
+# 	reader = csv.reader(csvfile, delimiter=';')
+# 	for row in reader:
+# 		genexam(*row)
+#
+# # Also generate a blank one. LaTex needs some content, so we use gibberish white space.
+# genexam('~','~','~')
 
+wb = load_workbook(filename='zulassungsliste.xlsx', read_only=True)
+sheets = wb.get_sheet_names()
+ws = wb[sheets[0]]
+
+startRow = 1
+matrikelCol = 1
+# nameRow = 1
+nameCol = 1
+# vornameRow = 1
+vornameCol = 1
+
+for i in range(1,ws.max_row+1):
+	for j in range(1, ws.max_column+1):
+		if 'matrikelnummer' == str(ws.cell(row=i,column=j).value).lower():
+			startRow = i
+			matrikelCol = j
+		elif 'name' == str(ws.cell(row=i, column=j).value).lower():
+			# nameRow = i
+			nameCol = j
+		elif 'vorname' == str(ws.cell(row=i, column=j).value).lower():
+			# vornameRow = i
+			vornameCol = j
+		# print(ws.cell(row=i,column=j).value)
+
+
+for i in range(startRow,ws.max_row+1):
+	# student = (ws.cell(row=i,column=matrikelCol).value, ws.cell(row=i,column=vornameCol).value, ws.cell(row=i,column=nameCol).value)
+	# print(student)
+	genexam(ws.cell(row=i, column=matrikelCol).value, ws.cell(row=i, column=vornameCol).value, ws.cell(row=i, column=nameCol).value)
