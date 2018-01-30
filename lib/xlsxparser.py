af17e753ef6fb1163d2003967b395ab6b5aaa64e
Peter Tröger <peter@troeger.eu>
2018-01-30 16:53:45 +0100

    Try to fix warnings

--- lib/xlsxparser.py
+++ lib/xlsxparser.py
@@ -6,35 +6,22 @@ def students(xlsx_file):
     sheets = wb.get_sheet_names()
     ws = wb[sheets[0]]
 
-    startRow = 1
-    matrikelCol = 1
-    nameCol = 1
-    vornameCol = 1
-
-    matrikelFound = False
-    nameFound = False
-    vornameFound = False
-
     # find the starting row for the data and the columns for id, name and first name
-    for i in range(1, ws.max_row + 1):
-        for j in range(1, ws.max_column + 1):
-            if str(ws.cell(row=i, column=j).value).lower() in ['matrikelnummer', 'matr-nr.']:
-                startRow = i + 1
-                matrikelCol = j
-                matrikelFound = True
-            elif 'name' == str(ws.cell(row=i, column=j).value).lower():
-                nameCol = j
-                nameFound = True
-            elif 'vorname' == str(ws.cell(row=i, column=j).value).lower():
-                vornameCol = j
-                vornameFound = True
-
-    if not (matrikelFound and nameFound and vornameFound):
-        raise RuntimeError("Could not find neccessary columns in {0}.".format(xlsx_file))
-
-    for i in range(startRow, ws.max_row + 1):
-        student_id = str(ws.cell(row=i, column=matrikelCol).value)
-        first_name = ws.cell(row=i, column=vornameCol).value
-        last_name = ws.cell(row=i, column=nameCol).value
-        if student_id != 'None':  # Might be None in empty rows
-            yield first_name, last_name, student_id
+    student_id_col = None
+    first_name_col = None
+    last_name_col = None
+    for row_index, row in enumerate(ws.iter_rows()):
+        if student_id_col and first_name_col and last_name_col:
+            student_id = str(ws.cell(row=row_index+1, column=student_id_col+1).value)
+            first_name = ws.cell(row=row_index+1, column=first_name_col+1).value
+            last_name = ws.cell(row=row_index+1, column=last_name_col+1).value
+            if student_id != 'None':  # Might be None in empty rows
+                yield first_name, last_name, student_id
+        else:
+            for col_index, cell in enumerate(row):
+                if str(cell.value).lower() in ['matrikelnummer', 'matr-nr.']:
+                    student_id_col = col_index
+                elif 'name' == str(cell.value).lower():
+                    last_name_col = col_index
+                elif 'vorname' == str(cell.value).lower():
+                    first_name_col = col_index
